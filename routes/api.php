34ea160ef2d99fed83513645cf18f2c27a9d5510
vinzy <rafipandya06@gmail.com>
2026-04-13 20:46:16 +0700

    memperbaiki api upload

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\GalleryController;
+use App\Http\Controllers\ContentController;
 
 // Endpoint Agregat untuk Dashboard
 // Bisa dites di http://localhost:8000/api/dashboard
@@ -11,3 +12,6 @@ Route::get('/dashboard', [DashboardController::class, 'index']);
 
 // Endpoint untuk Gallery
 Route::get('/gallery', [GalleryController::class, 'index']);
+
+// Endpoint untuk Upload Karya (bebas CSRF)
+Route::post('/upload', [ContentController::class, 'upload']);
