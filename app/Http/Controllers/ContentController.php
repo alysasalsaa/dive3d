6475a263b6cdf58b6b00d648ef603237eec8eb15
vinzy <rafipandya06@gmail.com>
2026-04-13 20:46:16 +0700

    memperbaiki api upload

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -10,8 +10,16 @@ class ContentController extends Controller
 {
     public function upload(Request $request){
         $request->validate([
-            "file"=> "required|file|mimes:glb,gltf,obj|max:102400",
+            "file"=> "required|file|max:102400",
             'title'=> 'required|string']);   
+
+        $extension = $request->file('file')->getClientOriginalExtension();
+        if (!in_array(strtolower($extension), ['glb', 'gltf', 'obj'])) {
+            return response()->json([
+                'message' => 'The file field must be a file of type: glb, gltf, obj.',
+                'errors' => ['file' => ['The file extension must be .glb, .gltf, or .obj.']]
+            ], 422);
+        }
 
     $path = $request->file('file')->store('uploads','public');
     $url = asset('storage/'.$path);
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route:: post('/upload',[ContentController::class,'upload']);
-
