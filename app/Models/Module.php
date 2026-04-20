c66a6097e9f8f4b9b36d08e0d4d1e18fce5b6caa
Ross <agustarossi03@gmail.com>
2026-04-21 05:24:34 +0700

    feat: integrate audio and narration API

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Module extends Model
 {
-    // Baris di bawah ini mengizinkan masuknya data secara massal
     protected $guarded = [];
+
+    // Relasi WBS 2.3.3: Satu Modul memiliki BANYAK titik POI (HasMany)
+    public function pois()
+    {
+        return $this->hasMany(PointOfInterest::class);
+    }
 }
