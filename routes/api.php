1a56828dff0981fa69c49c070eaaa4d2ba4bda56
Abyan <abyanbergasirmawan2006@gmail.com>
2026-04-20 14:17:58 +0700

    feat: add API login register

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,11 @@ use App\Http\Controllers\Api\GalleryController;
 use App\Http\Controllers\ContentController;
 use App\Http\Controllers\ModuleController;
 use App\Http\Controllers\UserProgressController;
+use App\Http\Controllers\Api\AuthController;
 
+// Auth Endpoints
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 // Endpoint Agregat untuk Dashboard
 // Bisa dites di http://localhost:8000/api/dashboard
@@ -24,3 +28,8 @@ Route::get('/modules', [ModuleController::class, 'index']);
 
 Route::get('/progress/{slug}', [UserProgressController::class, 'show']);
 Route::post('/progress', [UserProgressController::class, 'update']);
+
+// Protected Endpoints
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
